ad5fba9767df119d91b23a061f70322ca947e4eb
Andrea Nilsson <andreaycoral@icloud.com>
2025-01-16 20:54:15 +0000

    customize admin

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 # product view
@@ -7,6 +9,13 @@ def all_products(request):
     """
 
     products = Product.objects.all()
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria")
+                return redirect(reverse('products'))
 
     context = {
         'products': products,
